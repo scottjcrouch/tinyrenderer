949d4c5ab40d0e2febd740900a64e6e0f3f80430
Scott J. Crouch <scottjcrouch@gmail.com>
2019-04-07 14:29:54 +1000

    Lesson 1: Bresenham's Line Drawing Algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <cmath>
+#include <vector>
+#include <memory>
 
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor black = TGAColor(  0,   0,   0, 255);
@@ -17,23 +21,23 @@ void lesson0()
     image.write_tga_file("output.tga");
 }
 
-void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void drawLine(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     // To draw the line without gaps, we have to iterate over the longest
     // coordinate.  Therefore we check here to see if the segment is "steep",
     // i.e. the line segment is taller than it is wide.  If so, we will
     // un-steepen temporarily by flipping the coordinates, then un-flip them
     // when we finally draw.
     bool isSteep = false;
-    if (std::abs(x0-x1) < std::abs(y0-y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         isSteep = true;
     }
 
-    // Ensure the leftmost point is {x0,y0}.
-    if (x0 > x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    // Ensure p0 is the leftmost point
+    if (p0.x > p1.x) {
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
     }
 
     // Normally each iteration, we would increment x by 1 and y by dy/dx, then
@@ -41,11 +45,11 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     // calculation of dy/dx out of the loop (derror).  But we can further
     // eliminate needing to use floats altogether with some clever dimensional
     // analysis to eliminate some terms.
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
     int derror2 = std::abs(dy) * 2;
     int error2 = 0;
-    for (int x = x0, y = y0; x <= x1; x++) {
+    for (int x = p0.x, y = p0.y; x <= p1.x; x++) {
         if (isSteep) {
             image.set(y, x, color);
         } else {
@@ -53,7 +57,7 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
         }
         error2 += derror2;
         if (error2 > dx) {
-            y += ((y1 > y0) ? 1 : -1);
+            y += ((p1.y > p0.y) ? 1 : -1);
             error2 -= (dx * 2);
         }
     }
@@ -62,16 +66,40 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 // Draw a series of lines to draw a mesh
 void lesson1()
 {
-    TGAImage image(100, 100, TGAImage::RGB);
-    drawLine(20, 13, 40, 80, image, red);
-    drawLine(80, 40, 13, 20, image, green);
+    auto model(std::make_unique<Model>("obj/african_head.obj"));
+    const int width  = 800;
+    const int height = 800;
+    TGAImage image(width, height, TGAImage::RGB);
+
+    // For each face, draw lines connecting all three points of the triangle.
+    // The vertices are defined as floats in the range [-1.0, 1.0].  We obtain
+    // pixel coords from them by scaling to the height and width of the tga
+    // image.
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            Vec2i p0{int((v0.x + 1.0) * width / 2.0),
+                    int((v0.y + 1.0) * height / 2.0)};
+            Vec2i p1{int((v1.x + 1.0) * width / 2.0),
+                    int((v1.y + 1.0) * height / 2.0)};
+            drawLine(p0, p1, image, white);
+        }
+    }
     image.flip_vertically();
     image.write_tga_file("output.tga");
+}
+
+void lesson2()
+{
+    // ...
 }
 
 int main(int argc, char** argv)
 {
     // lesson0();
-    lesson1();
+    // lesson1();
+    lesson2();
     return 0;
 }
